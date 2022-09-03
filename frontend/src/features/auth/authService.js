dffb5f2d8586699d5fa78057df0a540f102ba9ce
Mark Winters <nulimitz360@gmail.com>
2022-09-03 12:33:43 -0600

    Finished login page

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -2,8 +2,20 @@ import axios from "axios";
 
 const API_URL = "/api/users/";
 
+//register user
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+//login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
@@ -19,6 +31,7 @@ const logout = () => {
 const authService = {
   register,
   logout,
+  login,
 };
 
 export default authService;
